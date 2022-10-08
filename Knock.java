595563b34972d4a55c1c9ae9ad7635ff5e185d1f
LifesMystery <arjunvsudn@gmail.com>
2022-10-07 21:25:27 -0500

    first commit

---
+++ Knock.java
@@ -0,0 +1,13 @@
+public class Knock
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Knock, knock, knock, \"Penny!\"");
+        System.out.println("Knock, knock, knock, \"Penny!\"");
+        System.out.println("Knock, knock, knock, \"Penny!\"");
+        System.out.println("Knock, knock, knock, \"Leonard!\"");
+        System.out.println("Knock, knock, knock, \"Leonard!\"");
+        System.out.println("Knock, knock, knock, \"Leonard!\"");
+        
+    }
+}
